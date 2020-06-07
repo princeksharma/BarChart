0081eec66407d09c5c7f32b47d0bdb0f7417052b
Prince Sharma <sharmaprince2271997@gmail.com>
2020-06-07 11:15:38 +0530

    Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import BarChart from './components/BarChart/BarChart';
+
+const data = [
+  {
+    name: 'Coca Cola',
+    val_1: 4000,
+    val_2: 2000,
+    mark_val: 3000,
+  },
+  {
+    name: 'Fanta ',
+    val_1: 700,
+    val_2: 900,
+    mark_val: 800,
+  },
+  {
+    name: 'Sprite',
+    val_1: 400,
+    val_2: 500,
+    mark_val: 900,
+  },
+  {
+    name: 'Cappy',
+    val_1: 500,
+    val_2: 500,
+    mark_val: 500,
+  },
+];
+
+const allKeys = ['val_1', 'val_2', 'mark_val'];
+
+const colors = {
+  val_1: 'green',
+  val_2: 'lightGreen',
+  mark_val: 'black',
+};
 
 function App() {
+  const [keys, setKeys] = useState(allKeys);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2>Hroizontal Bar Chart </h2>
+      <BarChart data={data} keys={keys} colors={colors} />
+
+      <div className='fields'>
+        {allKeys.map((key) => (
+          <div key={key} className='field'>
+            <input
+              id={key}
+              type='checkbox'
+              checked={keys.includes(key)}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setKeys(Array.from(new Set([...keys, key])));
+                } else {
+                  setKeys(keys.filter((_key) => _key !== key));
+                }
+              }}
+            />
+            <label htmlFor={key} style={{ color: colors[key] }}>
+              {key}
+            </label>
+          </div>
+        ))}
+      </div>
+    </React.Fragment>
   );
 }
 
